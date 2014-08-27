df692e7bdefd32db04f65a283de90e5d86ee0a90
Shlepanets <Shlepanets@gmail.com>
2014-08-27 16:02:06 +0400

    Revert "Ебусь с правильным редиректом"
    This reverts commit e98f43d2933e7a0494c9d0b940b0fd93e547ea66.

--- backend/blog_repost.php
+++ backend/blog_repost.php
@@ -31,16 +31,11 @@ if ((in_array($_SERVER['HTTP_USER_AGENT'], array(
 
 	<?
 	} else {
-  		//that's not Facebook
-		/* Redirect to a different page in the current directory that was requested */
-		$host  = $_SERVER['HTTP_HOST'];
-		$id	   = "?id=".$_GET['id']);
-		// $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		// $id = 'mypage.php';
-		// header("Location: http://$host$uri/$extra");
-		header("Location: http://$host/blog.php$id");
-		die();
-	}
+  //that's not Facebook
+	header('Location: <?echo "http://".$_SERVER['HTTP_HOST']."blog.php"?>');
+	die();
+
+}
 
  ?>
  <!-- social buttons -->
