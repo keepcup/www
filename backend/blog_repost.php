f5fcbe6358c462bea1a83d31ac300c386d6446c4
Shlepanets <Shlepanets@gmail.com>
2014-08-27 15:11:11 +0400

    Репосты работают
    Оформить в нормальные кнопки

--- backend/blog_repost.php
+++ backend/blog_repost.php
@@ -1,36 +1,36 @@
 <?
-if (in_array($_SERVER['HTTP_USER_AGENT'], array(
+if ((in_array($_SERVER['HTTP_USER_AGENT'], array(
   'facebookexternalhit/1.1 (+https://www.facebook.com/externalhit_uatext.php)',
   'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)'
-))) || (in_array($_SERVER['HTTP_USER_AGENT'], 'Twitterbot')) {
+))) || (in_array($_SERVER['HTTP_USER_AGENT'], array('Mozilla/5.0 (compatible; vkShare; +http://vk.com/dev/Share)')))) {
   //it's probably Facebook's bot
 	include "../db.php";
 	$select = $db->prepare("SELECT * FROM blog WHERE id=?");
 	$select->execute(array($_GET['id']));
-	$row = $select->fetchAll();
-	$select_count = $select->rowCount();			
+	$row = $select->fetch();
 	?>
 		<head>
 			<meta property="og:type"            content="article" /> 
-			<meta property="og:title"           content="<?echo $row[$i]['title']." ".$row[$i]['title_small']?>" />
+			<meta property="og:title"           content="<?echo $row['title']." ".$row['title_small']?>" />
 			<meta property="og:url"           	content="<?echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>" />
-			<meta property="og:image"           content="<?echo "http://".$_SERVER['HTTP_HOST']."/images/blog/images/".$row[$i]['photo']?>" />
-			<meta property="og:description"     content="<?echo $row[$i]['text']?>" />
+			<meta property="og:image"           content="<?echo "http://".$_SERVER['HTTP_HOST']."/images/blog/images/".$row['photo']?>" />
+			<meta property="og:description"     content="<?echo $row['text']?>" />
 		</head>
-			<!-- <div class="blog">
+			<div class="blog">
 				<div class="blog-belt">
 					<div class="blog-text_block">
-						<p class="blog-title"><span><?echo $row[$i]['title']?></span>
+						<p class="blog-title"><span><?echo $row['title']?></span>
 						<br>
-						<?echo $row[$i]['title_small']?></p>
+						<?echo $row['title_small']?></p>
 					</div>
-					<img class="blog-img" src="../images/blog/images/<?echo $row[$i]['photo']?>" alt="<?echo $row[$i]['url_name']?>">
+					<img class="blog-img" src="../images/blog/images/<?echo $row['photo']?>" alt="<?echo $row['url_name']?>">
 					<div class="blog-text-belt">
-						<p class="blog-text"><?echo $row[$i]['text']?></p>
+						<p class="blog-text"><?echo $row['text']?></p>
 					</div>
-			</div> -->
+			</div>
 
-	<?} else {
+	<?
+	} else {
   //that's not Facebook
 	header("Location: http://test.asartdesign.ru/blog_soc_test.php");
 	die();
